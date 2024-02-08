8a961de0d5a3f1f4e8777618a75e4f16b1bc049b
Anatoly Zemcov <Zemcovanatoly@yandex.ru>
2024-02-08 21:30:49 +0300

    Готовая работа

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Проектная работа Mesto
-
+https://github.com/Chechevism/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,24 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const cardList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+initialCards.forEach((item) => {
+  const card = createCard(item.name, item.link);
+  cardList.append(card);
+})
+
+function createCard(name, link) {
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.card__title').textContent = name;
+  cardElement.querySelector('.card__image').alt = name;
+  cardElement.querySelector('.card__image').src = link;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+  return cardElement;
+}
+
+function deleteCard(event) {
+  event.target.closest('.card').remove();
+}
